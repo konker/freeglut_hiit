dae9c79d6a558f238f2ff1c459705ae8051dc92a
konker <konker@luxvelocitas.com>
2014-01-31 19:04:55 +0200

    Add cross-android-gles2 cross compile directory and convenience rebuild script

---
+++ cross-android-gles2/rebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# cd to here
+cd "$(dirname "$0")"
+
+PATH=~/SDK/ndk-standalone-14/bin:$PATH
+cmake \
+    -D CMAKE_TOOLCHAIN_FILE=../android_toolchain.cmake \
+    -D CMAKE_INSTALL_PREFIX=~/SDK/ndk-standalone-14/sysroot/usr \
+    -D CMAKE_BUILD_TYPE=Debug \
+    -D FREEGLUT_GLES2=ON \
+    -D FREEGLUT_BUILD_DEMOS=NO \
+    .. \
+ && make -j4 && make install && rm -f ~/SDK/ndk-standalone-14/sysroot/usr/lib/libfreeglut-gles?.so*
+
+cd -
